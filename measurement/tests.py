e82b54c26b926e9a9dc304974f61a8c694d48345
batMaxCom <batman181194@gmail.com>
2023-04-05 18:38:51 +0300

    add pull script

--- measurement/tests.py
+++ measurement/tests.py
@@ -1,11 +1,9 @@
 from unittest import TestCase
 
-from rest_framework.test import APIClient
-
 
 class TestSampleView(TestCase):
     def test_view(self):
-        client = APIClient()
-        response = client.get('/api/sensors/')
-        print(response)
+        assert 1 == 1
+
+    def new_test_view(self):
         assert 1 == 1
